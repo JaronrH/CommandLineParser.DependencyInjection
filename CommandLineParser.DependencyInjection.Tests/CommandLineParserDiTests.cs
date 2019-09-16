0cd4e71d6f4adcc3cfb4eb50bfb4cecf38d3ad3d
Jaron Horst <jaron@sound-expressions.com>
2019-09-16 11:14:35 -0400

    Added fix for non-verb options.

--- CommandLineParser.DependencyInjection.Tests/CommandLineParserDiTests.cs
+++ CommandLineParser.DependencyInjection.Tests/CommandLineParserDiTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Reflection;
 using CommandLineParser.DependencyInjection.Interfaces;
 using CommandLineParser.DependencyInjection.Tests.Services;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +34,24 @@ namespace CommandLineParser.DependencyInjection.Tests
         {
             var service = ServiceProvider.GetRequiredService<ICommandLineParser<string>>();
             Assert.Equal("Unable to parse \"-filename testfile.txt\".", service.ParseArguments(new[] { "-filename", "testfile.txt" }));
+        }
+
+        [Fact]
+        public void HelpTests()
+        {
+            var name = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyTitleAttribute>()?.Title ?? Assembly.GetCallingAssembly().GetName().Name;
+            var version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? Assembly.GetCallingAssembly().GetName().Version.ToString();
+            var service = ServiceProvider.GetRequiredService<ICommandLineParser<string>>();
+            using (var writer = new StringWriter())
+            {
+                service.ParseArguments(new string[0], o => o.HelpWriter = writer);
+                Assert.Equal($"{name} {version}\r\nCopyright (C) 1 author\r\n\r\nERROR(S):\r\n  No verb selected.\r\n\r\nERROR(S):\r\n  No verb selected.\r\n\r\n  ask        Ask a question.\r\n\r\n  help       Display more information on a specific command.\r\n\r\n  version    Display version information.\r\n\r\n", writer.ToString());
+            }
+            using (var writer = new StringWriter())
+            {
+                service.ParseArguments(new [] {"ask", "--help"}, o => o.HelpWriter = writer);
+                Assert.Equal($"{name} {version}\r\nCopyright (C) 1 author\r\nUSAGE:\r\nDo you like green eggs and ham?:\r\n  CommandLineParserDiTests ask --like \"Green Eggs and Ham?\"\r\n\r\n  --like          (Default: false) Should we like this?\r\n\r\n  --help          Display this help screen.\r\n\r\n  --version       Display version information.\r\n\r\n  value pos. 0    Required. What do we like?\r\n\r\n", writer.ToString());
+            }
         }
     }
 }
--- CommandLineParser.DependencyInjection/CommandLineParser.cs
+++ CommandLineParser.DependencyInjection/CommandLineParser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using CommandLine;
 using CommandLineParser.DependencyInjection.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,12 +11,14 @@ namespace CommandLineParser.DependencyInjection
     public class CommandLineParser<TResult> : ICommandLineParser<TResult>
     {
         private static readonly Type ExecuteCommandLineOptionsInterfaceType = typeof(IExecuteCommandLineOptions<,>);
-        private readonly IEnumerable<ICommandLineOptions> _commandLineOptions;
+        private readonly Type[] _commandLineOptionTypes;
         private readonly IServiceProvider _serviceProvider;
+        private readonly IEnumerable<ICommandLineOptions> _commandLineOptions;
 
         public CommandLineParser(IEnumerable<ICommandLineOptions> commandLineOptions, IServiceProvider serviceProvider)
         {
             _commandLineOptions = commandLineOptions;
+            _commandLineOptionTypes = commandLineOptions.Select(i => i.GetType()).ToArray();
             _serviceProvider = serviceProvider;
         }
 
@@ -31,7 +34,9 @@ namespace CommandLineParser.DependencyInjection
                 ? new Parser()
                 : new Parser(configuration))
             {
-                var result = parser.ParseArguments(args, _commandLineOptions.Select(i => i.GetType()).ToArray());
+                var result = _commandLineOptionTypes.Count() == 1 && _commandLineOptionTypes.All(i => !i.GetCustomAttributes<VerbAttribute>().Any())
+                    ? parser.ParseArguments<object>(() => _commandLineOptions.First(), args)
+                    : parser.ParseArguments(args, _commandLineOptionTypes);
                 if (result.Tag == ParserResultType.Parsed)
                 {
                     var type =
